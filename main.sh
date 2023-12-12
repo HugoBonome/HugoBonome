9e7f7750ad3f38c6cbe5ddb5240f9f5601df21b8
HugoBonome <143456036+HugoBonome@users.noreply.github.com>
2023-12-11 22:00:16 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,3 +2,7 @@
 
 # Script para imprimir o nome da equipe do GitHub
 echo "Equipe do GitHub: Peaky Blinders" 
+git clone HugoBonome
+cd HugoBonome
+git config --global user.name "Hugo Bonome"
+git config --global user.email "hugo.zb@aluno.ifsc.edu.br"
